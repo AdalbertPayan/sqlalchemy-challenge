40b02845021d1792be3d460f71ff4cc1ad0d149d
AdalbertPayan <adalbertpayan@gmail.com>
2023-09-06 17:31:23 -0700

    update

---
+++ SurfsUp/app.py
@@ -0,0 +1,39 @@
+# Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from flask import Flask, jsonify
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
